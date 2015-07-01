16dc12a547e3a04c33ff290972d34e54bbb0d138
Shohei Aoki <shoaok@gmail.com>
2015-07-01 21:21:27 +0900

    initial commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,63 @@
+class ProductsController < ApplicationController
+  helper_method :sort_column, :sort_direction
+  
+  def index
+    @products = Product.order(sort_column + " " + sort_direction)
+  end
+  
+  def show
+    @product = Product.find(params[:id])
+  end
+  
+  def new
+    @product = Product.new
+  end
+  
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "Successfully created product."
+      redirect_to @product
+    else
+      render :action => 'new'
+    end
+  end
+  
+  def edit
+    @product = Product.find(params[:id])
+  end
+  
+  def update
+    @product = Product.find(params[:id])
+    if @product.update_attributes(product_params)
+      flash[:notice] = "Successfully updated product."
+      redirect_to @product
+    else
+      render :action => 'edit'
+    end
+  end
+  
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    flash[:notice] = "Successfully destroyed product."
+    redirect_to products_url
+  end
+  
+  private
+  
+  def sort_column
+    Product.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
+  def product_params
+    params.require(:product).permit(:name,:price,:released_at)
+  end
+
+end
+
+
